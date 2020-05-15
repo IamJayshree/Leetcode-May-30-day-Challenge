e65f31a4ab197bc4335f4d712e166b77e391640e
Jayshree <jayshreea222@gmail.com>
2020-05-15 12:39:40 +0530

    May 6

---
+++ Majority Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> mp;
+        
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]++;
+        
+        int n = nums.size();
+       
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]] > n/2)
+                return nums[i];
+       
+        }
+        
+        return 0;
+    }
+};
